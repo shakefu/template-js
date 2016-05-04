a84a940e9559404c027a0c40bbf76fd2e0989690
Jacob Alheid <shakefu@gmail.com>
2016-05-04 12:23:23 -0700

    api: Rejigger so it doesn't need ES2015.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict"
 // System
-const fs = require('fs')
+var fs = require('fs')
 
 // 3rd Party
-const template = require('lodash.template')
+var template = require('lodash.template')
 
 // Rendering settings
-const settings = {
+var settings = {
     escape: /{-([\s\S]+?)-}/g,
     evaluate: /{%([\s\S]+?)%}/g,
     interpolate: /{{([\s\S]+?)}}/g,
@@ -16,37 +16,35 @@ const settings = {
 /**
  * Template class for reusable contexts and cached files.
  */
-class Template {
-    constructor (filename, context) {
-        // Save the context for reuse in sub-templates
-        this.context = context || {}
-        this.context.include = this.render.bind(this)
+function Template (filename, context) {
+    // Save the context for reuse in sub-templates
+    this.context = context || {}
+    this.context.include = this.render.bind(this)
 
-        // Save the filename for the initial render
-        this.filename = filename
+    // Save the filename for the initial render
+    this.filename = filename
 
-        // Create a cache so we only read files once
-        this.cache = {}
+    // Create a cache so we only read files once
+    this.cache = {}
+}
+
+Template.prototype.getTemplate = function getTemplate (tmpl) {
+    if (!this.cache[tmpl]) {
+        this.cache[tmpl] = template(fs.readFileSync(tmpl), settings)
     }
+    return this.cache[tmpl]
+}
 
-    getTemplate (tmpl) {
-        if (!this.cache[tmpl]) {
-            this.cache[tmpl] = template(fs.readFileSync(tmpl), settings)
-        }
-        return this.cache[tmpl]
-    }
+Template.prototype.render = function render (tmpl, context) {
+    if (context) this.context = context
+    tmpl = this.getTemplate(tmpl)
+    tmpl = tmpl(this.context)
+    tmpl = tmpl.replace(/\n$/, '')
+    return tmpl
+}
 
-    render (tmpl, context) {
-        if (context) this.context = context
-        tmpl = this.getTemplate(tmpl)
-        tmpl = tmpl(this.context)
-        tmpl = tmpl.replace(/\n$/, '')
-        return tmpl
-    }
-
-    toString () {
-        return this.render(this.filename)
-    }
+Template.prototype.toString = function toString () {
+    return this.render(this.filename)
 }
 
 module.exports = Template
