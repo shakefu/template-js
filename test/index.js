d9d0b13d70f552b0f95ba86feba8085cea5e2575
Jacob Alheid <shakefu@gmail.com>
2016-08-05 14:50:34 -0700

    test: Create a test.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,16 @@
+"use strict"
+const chai = require('chai')
+
+const Template = require('../index')
+
+const should = chai.should()
 
 describe('Template', function () {
-    it("should have tests")
+    it("should work", () => {
+        let tmpl = new Template('test/test.tmpl', {})
+        tmpl.toString().should.equal('1\n    2\n    3\n4')
+    })
+    it("should have tests", () => {
+        true.should.equal(true)
+    })
 })
