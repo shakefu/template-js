d2586820521fe540aae77fcc010cc62afb402b2c
Jacob Alheid <shakefu@gmail.com>
2016-08-05 14:55:22 -0700

    test: More tests.

--- test/index.js
+++ test/index.js
@@ -10,6 +10,12 @@ describe('Template', function () {
         let tmpl = new Template('test/test.tmpl', {})
         tmpl.toString().should.equal('1\n    2\n    3\n4')
     })
+
+    it("should be fine", () => {
+        let tmpl = new Template('test/test3.tmpl', {})
+        tmpl.toString().should.equal('1\n    2\n    3\n4')
+    })
+
     it("should have tests", () => {
         true.should.equal(true)
     })
